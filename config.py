9f179692bdbdae91cba16427b66aa753240779d5
kavyansh34 <160215944+kavyansh34@users.noreply.github.com>
2025-11-10 23:56:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+# Shared configuration file for your bot and dashboard
+
+# --- i preferred binance API ---
+API_KEY = #your API key
+API_SECRET = #your API secret
+
+# --- Telegram Bot ---
+# Use bot father to build your telegram bot
+BOT_TOKEN = #your bot token
+CHAT_ID = #your chat id
+
+# --- Trading Symbol ---
+SYMBOL = "BTCUSDT" #any asset you wish
+
+TICKER_ASSETS = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "SOLUSDT", "XRPUSDT" ]
